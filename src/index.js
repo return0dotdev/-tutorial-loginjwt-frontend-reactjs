8f6d2aca802fe3c36bbd3a29731c8417762dff94
return0 <phoommin.naksenee@gmail.com>
2022-11-06 13:48:44 +0700

    first commit frontend login system with jwt

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Login from './Login';
+import Album from './Album';
+import Signup from './Signup';
+
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/album" element={<Album />} />
+      <Route path="/signup" element={<Signup />} />
+
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
